10ca5f9e6755780b59c4923b2e6d1227a8f9152e
JoaoHL <jao.luciano8@gmail.com>
2015-12-13 17:46:38 -0200

    Corrigido pequeno erro na linha 6

--- mapreduce-ic.py
+++ mapreduce-ic.py
@@ -3,4 +3,4 @@ import pyspark
 spark = pyspark.SparkContext()
 text = spark.textFile("hdfs://iniciacao-m:9090/user/hadoop/enwiki-samplefile.xml")
 counts = text.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-counts.saveAsTexrFile("hdfs://iniciacao-m:9090/user/hadoop/enwiki-samplefile-procd.txt")
+counts.saveAsTextFile("hdfs://iniciacao-m:9090/user/hadoop/enwiki-samplefile-procd.txt")
