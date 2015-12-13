2b52bc09dbf61f370a665cc502fc789de6729164
JoaoHL <jao.luciano8@gmail.com>
2015-12-13 17:41:03 -0200

    primeira versão do script usado na Google Cloud

---
+++ mapreduce-ic.py
@@ -0,0 +1,6 @@
+import pyspark
+
+spark = pyspark.SparkContext()
+text = spark.textFile("hdfs://iniciacao-m:9090/user/hadoop/enwiki-samplefile.xml")
+counts = text.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+counts.saveAsTexrFile("hdfs://iniciacao-m:9090/user/hadoop/enwiki-samplefile-procd.txt")
