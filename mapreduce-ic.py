3242e5992c72c03f35b68a7a8fb2f060dbabef43
root <root@cluster-1-m.c.hadoop-yarn-spark.internal>
2016-01-10 20:36:25 +0000

    Script de mapreduce totalmente funcional

--- mapreduce-ic.py
+++ mapreduce-ic.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 
 spark = SparkContext()
-text = spark.textFile("hdfs://iniciacao-m:10000/user/ic/enwiki.xml")
-wordcount = text.flatMap(lambda line: line.split(' ')).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-wordcount.saveAsTextFile("hdfs://iniciacao-m:10000/user/ic/out/enwiki-pyspark-out")
+text = spark.textFile("hdfs://cluster-1-m/user/ic/enwiki.xml")
+wordcount = text.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+wordcount.saveAsTextFile("hdfs://cluster-1-m/user/ic/enwiki-pyspark-out.txt")
